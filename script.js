589d27d2c1d088ceb4afe8adf51251e66fafb558
Lester <lesterong776@gmail.com>
2021-06-20 22:38:45 +0800

    Setup game() function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == "rock") {
+    if (playerSelection.toLowerCase() === "rock") {
        switch (computerSelection) {
            case "rock":
                return "It's a tie!";
@@ -25,7 +25,7 @@ function playRound(playerSelection, computerSelection) {
                 return "You Lose! Scissors beats Rock!"
                 break;
        }
-    } else if (playerSelection.toLowerCase() == "paper") {
+    } else if (playerSelection.toLowerCase() === "paper") {
         switch (computerSelection) {
             case "paper":
                 return "It's a tie!";
@@ -39,23 +39,52 @@ function playRound(playerSelection, computerSelection) {
                 return "You Lose! Scissors beats Paper!"
                 break;
         }
-    } else if (playerSelection.toLowerCase() == "scissors") {
+    } else if (playerSelection.toLowerCase() === "scissors") {
         switch (computerSelection) {
             case "scissors":
                 return "It's a tie!";
                 break;
  
-             case "paper":
-                return "You Win! Scissors beats Paper!"
+            case "paper":
+                return "You Win! Scissors beats Paper!";
                 break;
              
-             case "rock":
-                return "You Lose! Rock beats Scissors!"
+            case "rock":
+                return "You Lose! Rock beats Scissors!";
                 break;
         }
     }
 }
 
-function game() {
+let playerScore = 0;
+let computerScore = 0;
 
+function game() {
+    while (playerScore != 5 && computerScore != 5) { 
+        let player = prompt('Rock, Paper or Scissors?', '');
+        let computer = computerPlay();
+        let result = playRound(player, computer);
+        console.log(result);
+
+        switch (true) {
+            case (result.includes("Win")):
+                ++playerScore;
+                console.log("Your Score is " + playerScore);
+                console.log("The Computer's Score is " + computerScore);
+                break;
+
+            case (result.includes("Lose")):
+                ++computerScore;
+                console.log("Your Score is " + playerScore);
+                console.log("The Computer's Score is " + computerScore);
+                break;
+
+            case (result.includes("tie")):
+                console.log("Your Score is " + playerScore);
+                console.log("The Computer's Score is " + computerScore);
+                break;
+        } 
+    } 
+
+    return "Game Over!"
 }
