7fe46b2fff9003dbc7cf24239b2882b097d9ad6b
Lester <lesterong776@gmail.com>
2021-06-20 21:56:41 +0800

    Setup playRound function

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ function computerPlay() {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock") {
+       switch (computerSelection) {
+           case "rock":
+               return "It's a tie!";
+               break;
+
+            case "paper":
+                return "You Win! Rock beats Paper!"
+                break;
+            
+            case "scissors":
+                return "You Lose! Scissors beats Rock!"
+                break;
+       }
+    } else if (playerSelection.toLowerCase() == "paper") {
+        switch (computerSelection) {
+            case "paper":
+                return "It's a tie!";
+                break;
+ 
+             case "rock":
+                 return "You Win! Paper beats Rock!"
+                 break;
+             
+             case "scissors":
+                 return "You Lose! Scissors beats Paper!"
+                 break;
+        }
+    } else if (playerSelection.toLowerCase() == "scissors") {
+        switch (computerSelection) {
+            case "scissors":
+                return "It's a tie!";
+                break;
+ 
+             case "paper":
+                 return "You Win! Scissors beats Paper!"
+                 break;
+             
+             case "rock":
+                 return "You Lose! Rock beats Scissors!"
+                 break;
+        }
+    }
+    
+
+}
