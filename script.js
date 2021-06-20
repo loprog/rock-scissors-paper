194fb3d4ed109512439f18a54d0cf19f2b5f2ca1
Lester <lesterong776@gmail.com>
2021-06-20 21:37:17 +0800

    Set up computerPlay

---
+++ script.js
@@ -0,0 +1,11 @@
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3);
+    
+    if (num == 0) {
+        return "rock";
+    } else if (num == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
