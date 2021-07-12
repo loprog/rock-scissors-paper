f160a4f4aa53b2b44cbeb4830483a2820c7b1ae9
Lester <lesterong776@gmail.com>
2021-07-12 16:16:09 +0800

    Setup console game

--- script.js
+++ script.js
@@ -9,81 +9,64 @@ function computerPlay() {
         return "scissors";
     }
 }
+let outcome;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock") {
        switch (computerSelection) {
             case "rock":
-                return "It's a tie!";
+                outcome = "It's a tie!";
                 break;
 
             case "paper":
-                return "You Win! Rock beats Paper!"
+                outcome = "You Win! Rock beats Paper!"
                 break;
             
             case "scissors":
-                return "You Lose! Scissors beats Rock!"
+                outcome = "You Lose! Scissors beats Rock!"
                 break;
        }
     } else if (playerSelection.toLowerCase() === "paper") {
         switch (computerSelection) {
             case "paper":
-                return "It's a tie!";
+                outcome = "It's a tie!";
                 break;
  
              case "rock":
-                return "You Win! Paper beats Rock!"
+                outcome = "You Win! Paper beats Rock!";
                 break;
              
              case "scissors":
-                return "You Lose! Scissors beats Paper!"
+                outcome = "You Lose! Scissors beats Paper!";
                 break;
         }
     } else if (playerSelection.toLowerCase() === "scissors") {
         switch (computerSelection) {
             case "scissors":
-                return "It's a tie";
+                outcome = "It's a tie";
                 break;
  
             case "paper":
-                return "You Win! Scissors beats Paper!";
+                outcome = "You Win! Scissors beats Paper!";
                 break;
              
             case "rock":
-                return "You Lose! Rock beats Scissors!";
+                outcome = "You Lose! Rock beats Scissors!";
                 break;
         }
     }
-}
 
-let playerScore = 0;
-let computerScore = 0;
+    if (outcome.includes('Win')) {
+        ++playerScore;
+    } else if (outcome.includes('Lose')) {
+        ++computerScore;
+    }
 
-function game() { 
-    let player = prompt('Rock, Paper or Scissors?', '');
-    let computer = computerPlay();
-    let result = playRound(player, computer);
-    console.log(result);
-
-    switch (true) {
-        case (result.includes("Win")):
-            ++playerScore;
-            console.log("Your Score is " + playerScore);
-            console.log("The Computer's Score is " + computerScore);
-            break;
-
-        case (result.includes("Lose")):
-            ++computerScore;
-            console.log("Your Score is " + playerScore);
-            console.log("The Computer's Score is " + computerScore);
-            break;
-
-        case (result.includes("tie")):
-            console.log("Your Score is " + playerScore);
-            console.log("The Computer's Score is " + computerScore);
-            break;
-    } 
-    return "End of Round"
+    console.log(outcome);
+    console.log(`Your score is ${playerScore}`);
+    console.log(`The computer score is ${computerScore}`);
 }
 
 let rock = document.querySelector('#rock');
